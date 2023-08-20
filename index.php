d04b4314e37049716071f328f40ec5227ae91aa5
Noelbis FLores <noelbisflores18@outlook.es>
2023-08-20 01:49:28 -0400

    version 0.1

---
+++ index.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>SIRCAP</title>
+</head>
+<body>
+    
+<div class="row">
+    <div class="col-4">
+
+        <div class="container">
+            <form>
+                
+                <div class="form-group">
+                    <label>usuario</label>
+                    
+                    <input type="text" class="form-control" name="usuario" id="usuario" placeholder="introduzca usuario">
+                </div>
+
+                <div class="form-group">
+                    <label>contraseña</label>
+                    
+                    <input type="password" class="form-control" name="clave" id="clave" placeholder="introduzca contraseña">
+                </div>
+
+                    <button type="submit" class="btn btn-primary">ingresar</button>
+                
+        
+            </form>
+        </div>
+
+    </div>
+
+<div class="col-8">
+
+    <img src="imagenes/princ.png" alt="imagen del cmdnna">
+
+
+
+</div>
+
+</div>
+
+</body>
+</html>
+
